dbb40371513ba44aa6559c544d4603e7fcfc50e7
Boora-Raman <157504430+Boora-Raman@users.noreply.github.com>
2025-06-01 13:28:47 +0530

    Update backend.tf

--- Terraform-Deployment/backend.tf
+++ Terraform-Deployment/backend.tf
@@ -1,1 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "medusa-terraform-state-bucket-777"
+    key            = "terraform/medusa/dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "medusa-terraform-lock"
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
